7f4d3949afe7dee7e8dcf08799c6cfb193b72f0b
František Žiačik <ziacik@gmail.com>
2015-10-19 23:09:49 +0200

    Load local anycomment with the current url.

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,4 +1,6 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
-//# sourceMappingURL=popup.js.map
+chrome.tabs.query({ active:true }, function(tabs) {
+	var tab = tabs[0];
+	document.getElementById('frame').src = 'http://localhost:1337/#!/?room=' + tab.url;
+})
